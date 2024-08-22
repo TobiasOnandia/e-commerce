8d2f5b6336066aa0c12bc36ef21a4f34854bd517
Tobias Onandia <tobiasonandia0@gmail.com>
2024-08-22 16:49:30 -0300

    create slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Provider from './Providers/Providers'
 import Header from './components/Header'
+import { Slider } from './components/Slider'
 import { Main } from './components/main'
 
-export default function Home () {
+export default function Home() {
   return (
     <main className="h-screen bg-white">
       <Provider>
-        <Header/>
-        </Provider>
-        <Main />
+        <Header />
+      </Provider>
+      <Main />
     </main>
   )
 }
